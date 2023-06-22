98a623b2beb797992b6391f03143d2cef8974dd2
Damien O Beecroft <beec613@marianas.pnl.gov>
2023-06-22 16:07:22 -0700

    Still trying

---
+++ code/test/test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module purge
+module load cuda/11.4
+module load python/miniconda3.9
+source /share/apps/python/miniconda3.9/etc/profile.d/conda.sh
+conda activate jax-cuda
+echo $PATH
